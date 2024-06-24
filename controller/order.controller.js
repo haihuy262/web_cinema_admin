597301c066060c0784bba65649990828cbba57ee
haihuy262 <ng.haihuy262@gmail.com>
2024-06-24 20:19:59 +0700

    feat: add interface list order, details order, add image delete and update, add order controller

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,3 +1,6 @@
 exports.listOrder = async (req, res, next) => {
   res.render("../views/manager/order/listOrder.ejs");
 };
+exports.detailsOrder = async (req, res, next) => {
+  res.render("../views/manager/order/detailsOrder.ejs");
+};
--- routes/order.js
+++ routes/order.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var orderController = require("../controller/order.controller");
 
 router.get("/list", orderController.listOrder);
+router.get("/details/:id", orderController.detailsOrder);
 
 module.exports = router;
