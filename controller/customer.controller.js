928adf23e074dd5cd76b79e499005d799745cf2c
haihuy262 <ng.haihuy262@gmail.com>
2024-08-19 15:49:14 +0700

    style: user

--- controller/customer.controller.js
+++ controller/customer.controller.js
@@ -1,7 +1,15 @@
+const path = require("path");
+
 exports.listCustomer = async (req, res, next) => {
-  res.render("../views/manager/customer/listCustomer.ejs");
+  res.render("../views/manager/customer/listCustomer.ejs", {
+    layout: path.join(__dirname, "../layouts/dashboard.ejs"),
+  });
 };
+
 exports.history_tickets_user = async (req, res, next) => {
   const id = req.params.id;
-  res.render("../views/manager/customer/history_tickets.ejs", { id });
+  res.render("../views/manager/customer/history_tickets.ejs", {
+    id,
+    layout: path.join(__dirname, "../layouts/dashboard.ejs"),
+  });
 };
