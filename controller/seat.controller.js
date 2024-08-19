26e407624b13d5e7453d0401339fd0daa5b8297c
haihuy262 <ng.haihuy262@gmail.com>
2024-08-19 16:22:11 +0700

    style: seat

--- controller/seat.controller.js
+++ controller/seat.controller.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios");
+const path = require("path");
 
 exports.listSeat = async (req, res, next) => {
   const token = req.session.admin.token;
@@ -12,7 +13,12 @@ exports.listSeat = async (req, res, next) => {
     });
     const dataCinemas = response.data;
 
-    res.render("../views/seat/listSeat.ejs", { dataCinemas, token, apiUrl });
+    res.render("../views/seat/listSeat.ejs", {
+      dataCinemas,
+      token,
+      apiUrl,
+      layout: path.join(__dirname, "../layouts/dashboard.ejs"),
+    });
   } catch (error) {
     console.log(error);
   }
